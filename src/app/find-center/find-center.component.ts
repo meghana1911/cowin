a5f9ee873d6f89b097aa35b96029ce4af00e97db
krishnanmuthusubramanian <krishnan@primaze.com>
2021-07-02 18:31:40 +0530

    minor change

--- src/app/find-center/find-center.component.ts
+++ src/app/find-center/find-center.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-
+//https://github.com/meghana1911/cowin.git
 @Component({
   selector: 'app-find-center',
   templateUrl: './find-center.component.html',
